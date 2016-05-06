f6011dc78138358ce3afe8c81204373ae3d71881
liuming <1969489778@qq.com>
2016-05-06 15:35:18 +0800

    刘明
    二维码生成和扫描小工具

---
+++ QYQRcodeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QYQRcodeDemo
+//
+//  Created by 明刘 on 16/5/6.
+//  Copyright © 2016年 liuming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QYQRcodeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QYQRcodeDemo
+//
+//  Created by 明刘 on 16/5/6.
+//  Copyright © 2016年 liuming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
